cc899b28a66d585faefa8c1a0ea1b7072b27443a
Florian Wuermseer <wuermsee@hm.edu>
2025-04-13 13:28:34 +0200

    feat: extend attribute with priorities

--- Grw/Attribute.lean
+++ Grw/Attribute.lean
@@ -10,35 +10,43 @@ initialize
 
 structure Hint where
   name : Name
+  prio : Nat
   keys : Array DiscrTree.Key
   deriving Inhabited
 
-initialize dbEx : ScopedEnvExtension Hint Hint (DiscrTree Name) ←
+initialize dbEx : ScopedEnvExtension Hint Hint (DiscrTree (Name × Nat)) ←
   registerScopedEnvExtension {
     mkInitial := return {},
     ofOLeanEntry := fun dt n => do
       pure n
     toOLeanEntry := id
-    addEntry := fun dt n => dt.insertCore n.keys n.name,
+    addEntry := fun dt n => dt.insertCore n.keys (n.name, n.prio),
     finalizeImport := id,
   }
 
-private def convertTheoremToHint (name : Name) : MetaM Hint := do
+private def convertTheoremToHint (name : Name) (prio : Nat) : MetaM Hint := do
   let expr ← mkConstWithFreshMVarLevels name
   let type ← inferType expr
   let (_, _, type) ← forallMetaTelescope type
   trace[Meta.Tactic.grewrite.hints]m!"insert: {type}, {name}"
   let arr ← DiscrTree.mkPath type
-  pure ⟨name, arr⟩
+  pure ⟨name, prio, arr⟩
 
 -- For relations create new Attribute (e.g. grewrite_rel) but reuse dbEx
+syntax (name := grw) "grw" (num "%")? : attr
 
 initialize registerBuiltinAttribute {
   name := `grw
   descr := "Register theorems to be used in the proof search for the grewrite tactic"
-  add := fun name _ kind => do
-    let (hint, _) ← convertTheoremToHint name |>.run
-    dbEx.add hint kind
+  add := fun name stx kind => do
+    match stx with
+    | `(grw| grw $n:num %) =>
+      let (hint, _) ← convertTheoremToHint name n.getNat |>.run
+      dbEx.add hint kind
+    | `(grw| grw) =>
+      let (hint, _) ← convertTheoremToHint name 100 |>.run
+      dbEx.add hint kind
+    | _ => Elab.throwUnsupportedSyntax
 }
 
 end Attribute
