590b1cf70ac508c78f4c26d9a19ce0f6c10cb83d
Florian Wuermseer <wuermsee@hm.edu>
2025-04-22 14:30:57 +0200

    chore: copyright

--- Grw.lean
+++ Grw.lean
@@ -1,8 +1,13 @@
+/-
+  Copyright (c) 2025 Florian Würmseer. All rights reserved.
+  Released under Apache 2.0 license as described in the file LICENSE.
+  Authors: Florian Würmseer
+-/
+
 -- This module serves as the root of the `Grw` library.
 -- Import modules here that should be built as part of the library.
 import Grw.Morphism
 import Grw.Tactic
 import Grw.PaperTactic
-import Grw.Eauto
 import Grw.Attribute
 import Grw.ProofSearch
--- Grw/Morphism.lean
+++ Grw/Morphism.lean
@@ -1,3 +1,9 @@
+/-
+  Copyright (c) 2025 Florian Würmseer. All rights reserved.
+  Released under Apache 2.0 license as described in the file LICENSE.
+  Authors: Florian Würmseer
+-/
+
 import Grw.Attribute
 
 open Lean.Order
